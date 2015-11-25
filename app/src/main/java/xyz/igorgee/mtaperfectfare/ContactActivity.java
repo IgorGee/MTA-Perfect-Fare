7e7983084f6cc304462b0ee7d740c4d60eb432a7
Igor Goldvekht <goldvekht.igor@gmail.com>
2015-11-25 03:41:27 -0500

    Implement email support button

--- app/src/main/java/xyz/igorgee/mtaperfectfare/ContactActivity.java
+++ app/src/main/java/xyz/igorgee/mtaperfectfare/ContactActivity.java
@@ -1,19 +1,24 @@
 package xyz.igorgee.mtaperfectfare;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ContactActivity extends AppCompatActivity {
 
     @Bind(R.id.drawerLayout) DrawerLayout drawerLayout;
     @Bind(R.id.toolbar) Toolbar toolbar;
+    @Bind(R.id.email_button) Button emailButton;
     ActionBarDrawerToggle drawerToggle;
 
     @Override
@@ -35,6 +40,14 @@ public class ContactActivity extends AppCompatActivity {
             getSupportActionBar().setHomeButtonEnabled(true);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    @OnClick(R.id.email_button)
+    public void sendEmail(View view) {
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"Gee.Developers@gmail.com"});
+        startActivity(Intent.createChooser(emailIntent, "Send email using..."));
     }
 
     @Override
